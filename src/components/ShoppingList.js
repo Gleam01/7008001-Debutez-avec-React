a09db9dd63692f2b3c4e636a9a8fc3c442f803ba
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-07-07 18:34:33 +0100

    Exercise done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,28 @@
-import { plantList } from '../datas/plantList'
+import { plantList } from "../datas/plantList";
+const categories = [];
+plantList.forEach((plant) => {
+	if (!categories.includes(plant.category)) {
+		categories.push(plant.category);
+	}
+});
 
 function ShoppingList() {
-	return <ul></ul>
+  return (
+    <div>
+      <h2>Liste des catégories de plantes</h2>
+      <ul>
+        {categories.map((category, index) => {
+          return <li key={`${category}-${index}`}>{category}</li>;
+        })}
+      </ul>
+	  <h2>Liste des plantes</h2>
+      <ul>
+        {plantList.map((plant) => {
+          return <li key={plant.id}>{plant.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
