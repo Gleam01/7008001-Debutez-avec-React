96d5fed55bb337225940fb2b2ae6b36a112bbe9a
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-07-07 18:57:30 +0100

    Exercise done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -18,7 +18,7 @@ function ShoppingList() {
 			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
+						{plant.name} {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
 					</li>
 				))}
 			</ul>
