0eaf2d26c4fa1b3fe37b1f4e8d753a95cd376cf2
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-07-08 15:51:03 +0100

    Exercise done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -17,6 +17,7 @@ function Cart() {
 			<h2>Panier</h2>
 			<div>Monstera : {monsteraPrice}€</div>
 			<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+			<button onClick={() => updateCart(0)}>Vider</button>
 			<h3>Total : {monsteraPrice * cart}€</h3>
 		</div>
 	) : (
