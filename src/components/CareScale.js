fb7c41b79d0ec09654c563585aaa5cf5d2751551
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2025-07-08 15:00:04 +0100

    Exercise done

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,24 +1,44 @@
-import Sun from '../assets/sun.svg'
-import Water from '../assets/water.svg'
+import Sun from "../assets/sun.svg";
+import Water from "../assets/water.svg";
 
-function CareScale({ scaleValue, careType }) {
-	const range = [1, 2, 3]
-	const scaleType =
-		careType === 'light' ? (
-			<img src={Sun} alt='sun-icon' />
-		) : (
-			<img src={Water} alt='water-icon' />
-		)
+function handleCareScaleClick(e, careType, careValue) {
+  e.stopPropagation();
+  const alertSuffixe = careType === "light" ? "de lumière" : "d'arrosage";
+  let careQuantity = "";
+  if (careValue <= 1) {
+    careQuantity = "beaucoup";
+  } else if (careValue <= 2) {
+    careQuantity = "modérement";
+  } else {
+    careQuantity = "peu";
+  }
 
-	return (
-		<div>
-			{range.map((rangeElem) =>
-				scaleValue >= rangeElem ? (
-					<span key={rangeElem.toString()}>{scaleType}</span>
-				) : null
-			)}
-		</div>
-	)
+  alert(`Cette plante requiert ${careQuantity} ${alertSuffixe}`);
 }
 
-export default CareScale
+function CareScale({ scaleValue, careType }) {
+  const range = [1, 2, 3];
+  const scaleType =
+    careType === "light" ? (
+      <img src={Sun} alt="sun-icon" />
+    ) : (
+      <img src={Water} alt="water-icon" />
+    );
+
+  return (
+    <div>
+      {range.map((rangeElem) =>
+        scaleValue >= rangeElem ? (
+          <span
+            key={rangeElem.toString()}
+            onClick={(e) => handleCareScaleClick(e, careType, scaleValue)}
+          >
+            {scaleType}
+          </span>
+        ) : null
+      )}
+    </div>
+  );
+}
+
+export default CareScale;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import '../styles/Footer.css'
+import { useState } from "react";
+import "../styles/Footer.css";
 
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("");
 
-	return (
-		<footer className='lmj-footer'>
-			<div className='lmj-footer-elem'>
-				Pour les passionné·e·s de plantes 🌿🌱🌵
-			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
-		</footer>
-	)
+  function handleBlur(value) {
+    if (!value.includes("@")) {
+      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.");
+    }
+  }
+
+  return (
+    <footer className="lmj-footer">
+      <div className="lmj-footer-elem">
+        Pour les passionné·e·s de plantes 🌿🌱🌵
+      </div>
+      <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} onBlur={(e) => handleBlur(e.target.value)} />
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
